7a1e2dfff83d50281686bd4409c74c0e3344468f
Boris Kalekanov <b.e.kalekanov@gmail.com>
2023-01-22 11:06:23 +0200

    brow drop

--- rain.js
+++ rain.js
@@ -7,16 +7,26 @@ document.body.appendChild(canvas)
 var ctx = canvas.getContext("2d")
 
 var y = 0
+const chars = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ";
 function drow() {
+    y += 50
+    blackTush()
+    writeASimvol()
+}
 
+function writeASimvol(){
     ctx.fillStyle = "#0F0"
     ctx.font = "50px serif"
-    y += 50
     ctx.fillText(randomMatrixSimbol(), 30, y);
 }
+function blackTush(){
+ctx.fillStyle = "rgba(0,0,0,0.1)"
+ctx.fillRect(0,0,canvas.width,canvas.height)
+}
+
 setInterval(drow,100)
 function randomMatrixSimbol() {
-    const chars = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ";
+   
     const randomCharsIndex = getRandomInt(chars.length)
     return chars[randomCharsIndex]
 }
