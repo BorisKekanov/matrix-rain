2cab2ae18118df0823175cdab72b7e6b994c426c
Boris Kalekanov <b.e.kalekanov@gmail.com>
2023-01-22 11:44:58 +0200

    more drops

--- rain.js
+++ rain.js
@@ -9,27 +9,31 @@ var ctx = canvas.getContext("2d")
 var y = 0
 const chars = "ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ";
 function drow() {
-    if (y>canvas.height) {
-        y=0
+    if (y > canvas.height) {
+        y = 0
     }
     y += 50
     blackTush()
-    writeASimvol()
+    for (let x = 0; x < canvas.width; x+=50) {
+        writeASimvol(x) 
+        
+    }
+    
 }
 
-function writeASimvol(){
+function writeASimvol(x) {
     ctx.fillStyle = "#0F0"
     ctx.font = "50px serif"
-    ctx.fillText(randomMatrixSimbol(), 30, y);
+    ctx.fillText(randomMatrixSimbol(), x, y);
 }
-function blackTush(){
-ctx.fillStyle = "rgba(0,0,0,0.2)"
-ctx.fillRect(0,0,canvas.width,canvas.height)
+function blackTush() {
+    ctx.fillStyle = "rgba(0,0,0,0.2)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
-setInterval(drow,150)
+setInterval(drow, 500)
 function randomMatrixSimbol() {
-   
+
     const randomCharsIndex = getRandomInt(chars.length)
     return chars[randomCharsIndex]
 }
